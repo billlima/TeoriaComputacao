7e17238ce08498c5407f1d0cb7d5ff2051b1213b
Kelvin Alves <kalves@kopp.com.br>
2020-07-08 19:55:17 -0300

    coisas

--- src/trabalhoteoriacomputacao/GeradorFluxoNovo.java
+++ src/trabalhoteoriacomputacao/GeradorFluxoNovo.java
@@ -8,6 +8,7 @@ package trabalhoteoriacomputacao;
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
 import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
 import java.util.List;
 import modelos.Instrucao;
 import utils.InstrucaoTipoEnum;
@@ -17,7 +18,7 @@ import utils.InstrucaoTipoEnum;
  * @author kelvin
  */
 public class GeradorFluxoNovo {
-    
+
     private final List<Instrucao> instrucoes;
     private final String PARTIDA = "partida(partida)";
     private final String SETA = "-->";
@@ -28,104 +29,147 @@ public class GeradorFluxoNovo {
     private final String QUEBRA_LINHA = "\n";
     private final String INPUT = "/tmp/input";
     private final MermaidCli cliente;
-    
-    public GeradorFluxoNovo(List<Instrucao> lista)  {
+    private String ultimaOperacaoPrintada = PARTIDA;
+    private boolean ultimaOperacaoTeste = false;
+    private StringBuilder fluxo;
+    private List<String> instrucoesProcessadas;
+
+    public GeradorFluxoNovo(List<Instrucao> lista) {
         this.instrucoes = lista;
         this.cliente = new MermaidCli();
+        fluxo = new StringBuilder();
+        instrucoesProcessadas = new ArrayList<>();
     }
-    
+
     private boolean isOperacao(String tipo) {
         return InstrucaoTipoEnum.OPERACAO.getTipo().equals(tipo);
     }
-    
+
     private String getValor(String linha, Integer posicao) {
         return linha.split(" ")[posicao];
     }
-    
+
     private int getValorVerdadeiro(String linha) {
         return Integer.parseInt(linha.split(" ")[5]) - 1;
     }
-    
+
     private int getValorFalse(String linha) {
         return Integer.parseInt(linha.split(" ")[8]) - 1;
     }
-    
+
     private boolean naoEhParada(int posicao) {
         return posicao >= 0 && posicao < instrucoes.size();
     }
-    
+
     private String getTeste(String rotulo) {
         return rotulo + "{" + rotulo + "}";
     }
-    
+
     private String getOperacao(String rotulo) {
         return rotulo + "[" + rotulo + "]";
     }
-    
+
     private String getStrOperacao(String ultimaOperacao, String operacao, boolean ultimaOperacaoTeste) {
-        return ultimaOperacaoTeste ? "" : 
-                (ultimaOperacao + " " + SETA + " " + getOperacao(operacao) + QUEBRA_LINHA);
+        return ultimaOperacaoTeste ? ""
+                : (ultimaOperacao + " " + SETA + " " + getOperacao(operacao) + QUEBRA_LINHA);
     }
-    
+
     private String getCabecalho() {
         return GRAFICO + QUEBRA_LINHA + PARTIDA + QUEBRA_LINHA;
     }
-    
+
     private String getStrTeste(String ultimaOperacao, String operacao, boolean ultimaOperacaoTeste) {
-        return  ultimaOperacaoTeste ? "" : 
-                (ultimaOperacao + " " + SETA + " " + getTeste(operacao) + QUEBRA_LINHA);
-    } 
-    
+        return ultimaOperacaoTeste ? ""
+                : (ultimaOperacao + " " + SETA + " " + getTeste(operacao) + QUEBRA_LINHA);
+    }
+
     private String getStrTesteCasos(String origem, String opTipo, String destino) {
         return getTeste(origem) + " " + SETA + " " + opTipo + " " + destino + QUEBRA_LINHA;
     }
-    
+
     private String adicionarOperacaoTeste(String operacao, Instrucao instrucao, boolean operacaoVerdadeira) {
         int posicao = operacaoVerdadeira ? getValorVerdadeiro(instrucao.getStringLinha()) : getValorFalse(instrucao.getStringLinha());
         String tipo = operacaoVerdadeira ? OP_VERDADE : OP_FALSA;
         
         if (naoEhParada(posicao)) {
             String operacaoTeste = getValor(instrucoes.get(posicao).getStringLinha(), 2);
+            
+            if (!operacaoVerdadeira) {
+                addLinha(instrucoes.get(posicao));
+            }
             return getStrTesteCasos(operacao, tipo, operacaoTeste);
         } else {
+            //tratar parada teste
             return getStrTesteCasos(operacao, tipo, PARADA);
         }
     }
-    
+
     private void criarArquivo(String fluxo) throws FileNotFoundException, UnsupportedEncodingException {
         PrintWriter writer = new PrintWriter(INPUT, "UTF-8");
         writer.print(fluxo);
         writer.close();
     }
-    
-    public boolean gerar() throws FileNotFoundException, UnsupportedEncodingException, Exception {
-        StringBuilder fluxo = new StringBuilder();
+
+    public void addLinha(Instrucao instrucao) {
+        for(String s : instrucoesProcessadas) {
+            System.out.println("L:" + s);
+        }
         
-        fluxo.append(getCabecalho());
-        String ultimaOperacaoPrintada = PARTIDA;
-        boolean ultimaOperacaoTeste = false;
+        System.out.println("Processando: " + instrucao.getStringLinha());
         
-        for (Instrucao instrucao : instrucoes) {
+        if (instrucoesProcessadas.contains(instrucao.getStringLinha())) {
+            
+            for (Instrucao i : instrucoes) {
+                if (!instrucoesProcessadas.contains(i.getStringLinha())) {
+                    addLinha(i);
+                }
+            }
+            
+            return;
+        } else {
+            
             if (isOperacao(instrucao.getTipo())) {
                 String operacao = getValor(instrucao.getStringLinha(), 2);
                 fluxo.append(getStrOperacao(ultimaOperacaoPrintada, operacao, ultimaOperacaoTeste));
                 ultimaOperacaoPrintada = operacao;
                 ultimaOperacaoTeste = false;
+                instrucoesProcessadas.add(instrucao.getStringLinha());
+                
+                int proximo = (Integer.parseInt(getValor(instrucao.getStringLinha(), 4)) - 1);
+                
+                if (naoEhParada(proximo)) {
+                    addLinha(instrucoes.get(proximo));
+                } else {
+                    //pensar na parada
+                    
+                }
             } else {
                 String operacao = getValor(instrucao.getStringLinha(), 2);
-                
+
                 fluxo.append(getStrTeste(ultimaOperacaoPrintada, operacao, ultimaOperacaoTeste));
+                
+                instrucoesProcessadas.add(instrucao.getStringLinha());
+                
                 fluxo.append(adicionarOperacaoTeste(operacao, instrucao, true));
                 fluxo.append(adicionarOperacaoTeste(operacao, instrucao, false));
-                
+
                 ultimaOperacaoPrintada = operacao;
                 ultimaOperacaoTeste = true;
             }
         }
-        
+    }
+
+    public boolean gerar() throws FileNotFoundException, UnsupportedEncodingException, Exception {
+        fluxo.append(getCabecalho());
+
+        addLinha(instrucoes.get(0));
+
+//        instrucoes.forEach((instrucao) -> {
+//            addLinha(instrucao);
+//        });
         criarArquivo(fluxo.toString());
-        
+
         return cliente.gerarImagemFluxo(INPUT).sucesso();
     }
-    
+
 }
